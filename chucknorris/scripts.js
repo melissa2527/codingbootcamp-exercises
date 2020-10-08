34cac56c6071522e8a8b8f12ad266646c0604c85
annie2527 <you@example.com>
2020-10-08 11:33:42 +0200

    chuck

---
+++ chucknorris/scripts.js
@@ -0,0 +1,36 @@
+'use strict';
+
+// const wrapper = document.querySelector('.wrapper');
+// const btnGenerator = document.querySelector('.button');
+// const quote = document.querySelector('.quote');
+
+// const getJoke = () => {
+//     fetch('https://api.chucknorris.io/jokes/random')
+//     .then(response => response.json())
+//     .then(json =>  { 
+//         quote.innerHTML = `<img src="${json.icon_url}" />`
+//         quote.innerHTML += json.value
+//     });
+// }
+
+// btnGenerator.addEventListener('click', getJoke)
+
+// let students = ['Mark', 'Annie', 'Joe', 'Lara', 'Jenna', 'Andy', 'Donna', 'Chris'];
+ 
+// students.sort();
+// students.unshift('Jacob');
+// students = students.reverse();
+
+
+
+// go to location
+// load passenger
+// load passenger
+// go to location
+// unloadfirst/ unload last 
+
+
+let array = [];
+array.push('red', 'green');
+array.push('blue', 'black');
+array = array.pop();
